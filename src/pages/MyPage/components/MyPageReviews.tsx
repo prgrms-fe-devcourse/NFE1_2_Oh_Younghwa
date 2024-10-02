5951b15523ed08ae0c636b1b302c5dd6f61bdd21
JOEIH <joh73459@gmail.com>
2024-10-02 20:12:00 +0900

    refator: develop에서 수정된 내용 가져옴

--- src/pages/MyPage/components/MyPageReviews.tsx
+++ src/pages/MyPage/components/MyPageReviews.tsx
@@ -1,18 +1,39 @@
-import Review from '../../MovieDetailPage/components/ReviewContainer';
-import { MoviePost, Post } from '../../TimelinePage/model/article';
+import { useEffect, useState } from 'react';
+
+import Review from '../../MovieDetailPage/components/Review';
+import { MoviePost } from '../../TimelinePage/model/article';
 import { getAllReviews } from '../api/userApi';
 
 import '../../MovieDetailPage/scss/Review.scss';
 
-export default function MyPageReviews() {
+type ReviewListProps = {
+  title: string;
+};
+export default function ReviewList() {
+  const [data, setData] = useState<MoviePost[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getAllReviews('66f51699c7562307e3bd6223');
+      setData(result);
+      console.log('🚀 ~ ReviewList ~ data:', result);
+    };
+
+    fetchData();
+  }, []);
+  // const data = getAllReviews('66f51699c7562307e3bd6223');
+  // console.log('🚀 ~ ReviewList ~ data:', data);
+  // const sortedByLikesLength = data?.sort((a, b) => b.likes.length - a.likes.length);
+  // console.log('🚀 ~ ReviewList ~ sortedByLikesLength:', sortedByLikesLength);
+
   return (
     <div className="review-wrapper">
-      {/* {reivewList?.map((review, index) => (
+      {/* {data?.map((review, index) => (
         <Review
           key={index}
           rating={review.rating}
           review={review.review}
-          author={userId}
+          author={review.author}
           authorId={review.authorId}
           channelId={review.channelId}
           postId={review.postId}
